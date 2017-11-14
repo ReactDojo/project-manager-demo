8cdab3a1988a4817be446d0e3ae3e02192d24b86
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-14 12:57:55 -0800

    Removed username from Users schema for now

--- functions/schema/User.js
+++ functions/schema/User.js
@@ -3,10 +3,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
     firstName: {
         type: String,
         required: true
