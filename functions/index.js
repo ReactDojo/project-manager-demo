ba11996a9974b2c20d89b9249298ca64bc19f77b
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-14 12:56:53 -0800

    Added additional Access Control headers to allow various methods

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Apply Access Headers to all requests (enable CORS)
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     next();
 });
 
