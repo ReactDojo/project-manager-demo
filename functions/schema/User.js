fea4701dabd5a46a4ecb2397fa6d9845d37561ec
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-14 09:44:31 -0800

    Created new schema for Users

---
+++ functions/schema/User.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    mobile: { 
+        type: String,
+        required: true
+    },
+    home: { 
+        type: String,
+        required: true
+    },
+    company: { 
+        type: String,
+        required: true
+    },
+    work: { 
+        type: String,
+        required: true
+    },
+    note: { 
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', User);
