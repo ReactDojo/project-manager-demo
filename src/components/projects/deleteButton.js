2024b5581dd8cd6832e422f6bdf98c274ae04840
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-18 16:49:59 -0800

    Altered and updated to deleteProjectFromDB

--- src/components/projects/deleteButton.js
+++ src/components/projects/deleteButton.js
@@ -2,28 +2,22 @@ import React, { Component } from 'react';
 import Popconfirm from '../feedback/popconfirm';
 import Button from '../uielements/button';
 import notification from '../notification';
+import { deleteProjectFromDB } from '../../containers/Projects/dataHelper';
 
 export default class DeleteButton extends Component {
   render() {
     const { project, deleteProject } = this.props;
-    let name = '';
-    if (project.firstName) {
-      name = `${project.firstName} `;
-    }
-    if (project.lastName) {
-      name = `${name}${project.lastName}`;
-    }
-    if (!name) {
-      name = 'No Name';
-    }
+    let name = project.title || 'Untitled Project';
     return (
       <Popconfirm
         title={`Sure to delete ${name}?`}
         okText="DELETE"
         cancelText="No"
         onConfirm={() => {
-          notification('error', `Deleted ${name}`, '');
-          deleteProject(project.id);
+          deleteProject(project._id);
+          deleteProjectFromDB(project, (response) => {
+            notification(response.type, response.message, response.description);
+          });
         }}
       >
         <Button icon="close" type="button" className="isoDeleteBtn" />
