d1201fdcb531fed6c381f02ad43162e2ffc3f126
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-18 16:54:39 -0800

    Altered background image, and alpha settings of overlay

--- src/containers/Page/signin.style.js
+++ src/containers/Page/signin.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { palette } from 'styled-theme';
-import bgImage from '../../image/image1.jpg';
+import bgImage from '../../image/image7.png';
 import WithDirection from '../../config/withDirection';
 
 const SignInStyleWrapper = styled.div`
@@ -19,7 +19,7 @@ const SignInStyleWrapper = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
-    background-color: rgba(0, 0, 0, 0.6);
+    background-color: rgba(0, 0, 0, 0.2);
     position: absolute;
     z-index: 1;
     top: 0;
