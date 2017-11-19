a11db0bae8dd531da66d7014622d820998b5e5a0
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-18 16:51:57 -0800

    Small variable name changes

--- src/components/projects/singleView.js
+++ src/components/projects/singleView.js
@@ -3,10 +3,18 @@ import { ProjectCardWrapper } from './projectCard.style';
 
 export default class SingleProjectView extends Component {
   render() {
-    const { project, otherAttributes } = this.props;
-    const name = project.name ? project.name : 'No Name';
+    const { project } = this.props;
+    const title = project.title ? project.title : 'Untitled Project';
     const extraInfos = [];
-    otherAttributes.forEach(attribute => {
+    const details = [
+      { value: 'title', title: 'Title' },
+      { value: 'startdate', title: 'Start Date' },
+      { value: 'enddate', title: 'End Date' },
+      { value: 'rateofpay', title: 'Pay Rate' },
+      { value: 'status', title: 'Status' },
+      { value: 'notes', title: 'Notes' },
+    ];
+    details.forEach(attribute => {
       const value = project[attribute.value];
       if (value) {
         extraInfos.push(
@@ -26,7 +34,7 @@ export default class SingleProjectView extends Component {
             {project.avatar ? <img alt="#" src={project.avatar} /> : ''}
           </div>
           <h1 className="isoPersonName">
-            {name}
+            {title}
           </h1>
         </div>
         <div className="isoProjectInfoWrapper">
