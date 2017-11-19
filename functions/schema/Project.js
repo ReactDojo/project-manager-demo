991bef9d6eb62ceab35e427bfa133a56212a76fa
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-18 16:45:56 -0800

    Altered project schema to include more input values

--- functions/schema/Project.js
+++ functions/schema/Project.js
@@ -3,16 +3,34 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Project = new Schema({
-    name: {
+    title: {
         type: String,
         required: true
     },
-    description: {
+    avatar: {
+        type: String,
+        required: false
+    },
+    startdate: {
+        type: Date,
+        required: true
+    },
+    enddate: { 
+        type: Date,
+        required: true
+    },
+    rateofpay: {
+        type: Number,
+        required: true
+    },
+    status: {
         type: String,
         required: true
     },
-    startdate: { type: Date },
-    enddate: { type: Date }
+    notes: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Project', Project);
